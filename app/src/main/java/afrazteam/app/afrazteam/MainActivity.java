45a2f1fd645f34909a8295d1446ae2f33bc21416
saman <8084saman@gmail.com>
2023-07-07 17:57:23 +0330

    create slider and uses permission <Ienternt> application.

--- app/src/main/java/afrazteam/app/afrazteam/MainActivity.java
+++ app/src/main/java/afrazteam/app/afrazteam/MainActivity.java
@@ -1,13 +1,47 @@
 package afrazteam.app.afrazteam;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.AnimationTypes;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageSlider imageSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        bindViews();
+        fillSliderData();
+
+    }
+
+    private void fillSliderData() {
+        List<SlideModel> imageList = new ArrayList<>();
+
+        imageList.add(new SlideModel(R.drawable.img_slider3, "And people do that.", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.img_slider1, "The animal population decreased by 58 percent in 42 years.", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.img_slider2, "Elephants and tigers may become extinct.", ScaleTypes.FIT));
+
+        imageSlider.setImageList(imageList);
+        imageSlider.setSlideAnimation(AnimationTypes.BACKGROUND_TO_FOREGROUND);
+        imageSlider.setAnimation(imageSlider.getAnimation());
+    }
+
+    private void bindViews() {
+        imageSlider = findViewById(R.id.image_slider);
     }
 }
